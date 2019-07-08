b5adf385a5ecad6a7a6606dcf613c911457bbe47
Jules Youngberg <julesdayoungberg@gmail.com>
2019-07-07 20:34:28 -0700

    switched to threejs and meyda for performance

--- sketch.js
+++ sketch.js
@@ -1,78 +1,73 @@
-let ICOSUBDIVISION = 2
-let offset = 0.4, rotation = 0, level = 0
-let ico, mic, cam
+var $container = document.getElementById("container");
+var renderer = new THREE.WebGLRenderer({antialias: true});
+var camera = new THREE.PerspectiveCamera(80,1,0.1,10000);
+var scene = new THREE.Scene();
 
-function createIcosahedron() {
-  return new Icosahedron(ICOSUBDIVISION)
+scene.add(camera);
+renderer.setSize(800, 800);
+$container.append(renderer.domElement);
+
+///////////////////////////////////////////////
+
+// Camera
+camera.position.z = 200;
+
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+// Material
+var pinkMat = new THREE.MeshPhongMaterial({
+  color      :  new THREE.Color("rgb(75,35,213)"),
+  emissive   :  new THREE.Color("rgb(100,100,255)"),
+  specular   :  new THREE.Color("rgb(100,100,255)"),
+  shininess  :  10,
+  shading    :  THREE.FlatShading,
+  transparent: 1,
+  opacity    : 1
+});
+
+var L1 = new THREE.PointLight( 0xffffff, 1);
+L1.position.z = 100;
+L1.position.y = 100;
+L1.position.x = 100;
+scene.add(L1);
+
+var L2 = new THREE.PointLight( 0xff0000, 0.8);
+L2.position.z = 200;
+L2.position.y = 50;
+L2.position.x = -100;
+scene.add(L2);
+
+// IcoSphere -> THREE.IcosahedronGeometry(80, 1) 1-4
+var ico = new THREE.Mesh(new THREE.IcosahedronGeometry(75,3), pinkMat);
+ico.rotation.z = 0.5;
+scene.add(ico);
+
+noise.seed(Math.random());
+
+let originalVertices;
+let offset = 0;
+
+const f = n => n * 2 + offset;
+
+function update(){
+   // ico.rotation.x+=2/100;
+   // ico.rotation.y+=2/100;
+
+   if (!originalVertices) originalVertices = ico.geometry.vertices;
+   ico.geometry.vertices.forEach((vertex, i) => {
+     const p = vertex.normalize();
+     const r = noise.perlin3(f(p.x), f(p.y), f(p.z)) * 60 + 40;
+     p.multiplyScalar(r*2);
+   });
+   ico.geometry.verticesNeedUpdate = true;
+   offset += 0.005;
 }
 
-function setup() {
-  createCanvas(window.innerWidth, window.innerHeight, WEBGL)
-  // smooth(10)
-  translate(width/2, height/2, 0)
-  cam = createEasyCam()
-  cam.zoom(1000)
-  ico = createIcosahedron()
-  mic = new p5.AudioIn()
-  mic.start()
+// Render
+function render() {
+  requestAnimationFrame(render);
+  update();
+  renderer.render(scene, camera);
 }
 
-function draw() {
-  background(0)
-  rotateY(degrees(PI) + rotation * 0.3)
-  rotateX(degrees(HALF_PI) + rotation * 1.190)
-
-  for (let i = 0; i < ico.particles.length; i += 3) {
-    const f1 = ico.particles[i].copy()
-    const f2 = ico.particles[i + 1].copy()
-    const f3 = ico.particles[i + 2].copy()
-    colorMode(HSB, 360, 100, 100)
-
-    // noFill()
-    let c = color(
-      225 - (f2.x + f1.y + f3.z) / 10,
-      100,
-      120  + abs(f2.y + f1.z + f3.x) / 4
-    )
-    strokeWeight(1)
-    stroke(c)
-    fill(c)
-
-    beginShape()
-    vertex(f1.x, f1.y, f1.z)
-    vertex(f2.x, f2.y, f2.z)
-    vertex(f3.x, f3.y, f3.z)
-    endShape(CLOSE)
-  }
-
-  const currentLevel = mic.getLevel()
-  // updateLevel(currentLevel)
-  // const maxHeight = map(level, 0, 1, 50, 120)
-  ico.terrain(offset)//, maxHeight)
-  // make the terrain move with the sound
-  offset += map(currentLevel, 0, 1, 0.000001, 0.15) // 0.01
-  rotation += 0.01 // rotation
-}
-
-function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    if (ICOSUBDIVISION === 5) return
-    ICOSUBDIVISION += 1
-  } else if (keyCode === DOWN_ARROW) {
-    if (ICOSUBDIVISION === 0) return
-    ICOSUBDIVISION -= 1
-  }
-  ico = createIcosahedron()
-}
-
-function touchStarted() {
-  if (getAudioContext().state !== 'running') {
-    getAudioContext().resume();
-  }
-}
-
-function updateLevel(currentLevel) {
-  const q = 0.3
-  const newLevel = (q * level) + ((1 - q) * currentLevel)
-  level = newLevel
-}
+render();
